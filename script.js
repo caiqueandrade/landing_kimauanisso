c830dd1914c2a6facb46da6e5ff20066ed4e39e4
Caique Andrade <caiquembandrade@gmail.com>
2017-05-23 06:18:18 -0300

    Atualização geral após mudança de planos

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
+// function trocarSlide(){
+//     slides[slideActive].classList.remove('active');
+//     slideActive++;
+//     if(slideActive == slides.length){
+//         slideActive = 0;
+//     }
+//     slides[slideActive].classList.add('active');
+// }
 
+function trocarTexto(){
+    textos[textoActive].classList.remove('active');
+    textoActive++;
+    if(textoActive == textos.length){
+        textoActive = 0;
+    }
+    textos[textoActive].classList.add('active');
+}
+
+function mover(){
+    var elemento = document.getElementById('bar');
+    var width = 1;
+    var id = setInterval(frame, 50);
+    function frame(){
+        if (width >= 100) {
+            clearInterval(id);
+        }
+        else {
+            width++;
+            elemento.style.width = width + '%';
+        }
+    }
+}
